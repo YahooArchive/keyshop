093e25225cd9de49ea783d8b1f3c13f058985ad3
David Leon Gil <dgil@yahoo-inc.com>
2015-03-30 14:51:25 -0700

    Removed Go1.5-only TLS ciphersuites.

--- ks/cmd/ks/main.go
+++ ks/cmd/ks/main.go
@@ -126,9 +126,11 @@ func main() {
 				*/
 			},
 			CipherSuites: []uint16{
+				/* go1.5 only:
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				*/
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			},
 			PreferServerCipherSuites: true,
