67600c3dda8db0d6bbde7691d5569d1d1a26ed15
Dmitry Savintsev <dsavints@yahoo-inc.com>
2015-04-14 16:40:02 +0200

    add doc.go as placeholder for package description

---
+++ ks/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2015 Yahoo
+// Author:  Dmitry Savintsev (dsavints@yahoo-inc.com)
+// License: Apache 2
+
+// Package ks implements a "keyshop" - a tiny keyserver stub (certificate authority)
+// provided for testing the E2E extension.
+package ks
